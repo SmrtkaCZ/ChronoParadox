3d4664070ab4fc2410b7deebeb20820503cf048c
SmrtkaCZ <milan.kristen@seznam.cz>
2023-11-24 21:01:02 +0100

    pohyb,skok

--- Assets/skripty/PohybHlHrdiny.cs
+++ Assets/skripty/PohybHlHrdiny.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PohybHlHrdiny : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Možnosti pohybu")]
+    [SerializeField]
+    public int rychlost;
+    public float nahoru;
+
+    Rigidbody teleso;
+    Vector3 pohybFinal;
+    private int pocetbodu;
+    private bool skociljsem = true;
+    private bool con = false;
+
+
     void Start()
     {
-        //Ahoj
+        teleso = GetComponent<Rigidbody>();
+        Time.timeScale = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        teleso.AddForce(pohybFinal * rychlost);
+    }
+
+    public void Movement(InputAction.CallbackContext kontext)
+    {
+        Vector2 pomocny = kontext.ReadValue<Vector2>();
+        pohybFinal = new Vector3(pomocny.x, 0f, pomocny.y);
+    }
+    public void Jump(InputAction.CallbackContext kontext)
+    {
+        if (skociljsem && kontext.performed)
+        {
+            teleso.AddForce(Vector3.up * nahoru);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            skociljsem = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            skociljsem = false;
+        }
     }
 }
